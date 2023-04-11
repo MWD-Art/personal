d87b4929e192c96afffdc6d6f7d392444cb62c3b
MWD-Art <marc@mwd-art.de>
2023-04-11 08:30:43 +0200

    initial

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,21 @@
-const eleventySass = require("@11tyrocks/eleventy-plugin-sass-lightningcss");
+const eleventySass = require('@11tyrocks/eleventy-plugin-sass-lightningcss');
+const { DateTime } = require('luxon');
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(eleventySass);
 
+  eleventyConfig.addFilter('postDate', (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+  });
+
+  eleventyConfig.addShortcode('year', () => `${new Date().getFullYear()}`);
+
+  eleventyConfig.addPassthroughCopy('./src/assets/');
+
   return {
     dir: {
-      input: "src",
-      output: "public",
+      input: 'src',
+      output: 'public',
     },
   };
 };
---
+++ src/_data/meta.js
@@ -0,0 +1,10 @@
+// Located in _data/meta.js
+module.exports = {
+  // NOTE: `process.env.URL` is provided by Netlify, and may need
+  // adjusted pending your host
+  url: process.env.URL || 'http://localhost:8080',
+  siteName: 'MWD Art',
+  siteDescription:
+    'Marc Weber ist ein Frontend/Webentwickler, UI/UX Designer aus Marburg in Hessen Deutschland.',
+  authorName: 'Marc Weber',
+};
